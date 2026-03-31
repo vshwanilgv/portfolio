b17edca051bee9dab245b294d175f0b16243b25d
vshwanilgv <vishwanibhagya2002@gmail.com>
2026-03-31 18:56:09 +0530

    feat: refactor Clarity tracking script integration in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,20 +24,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-
-      </head>
-
       <body className="antialiased">
-        <Script id="clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "w28bzdqy47");
-          `}
-        </Script>
+        <Script
+          id="ms-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "w28bzdqy47");
+            `,
+          }}
+        />
         <Navbar />
         <main>{children}</main>
         <Footer />
